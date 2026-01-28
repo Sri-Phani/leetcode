024349bb096c4737542e6fc8942c001e9925bd76
PHANI GUTTULA <107171342+Sri-Phani@users.noreply.github.com>
2026-01-28 22:51:45 +0530

    Time: 3 ms (37.19%) | Memory: 46.9 MB (15.09%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -6,12 +6,12 @@ class Solution {
             mpp.put(it,mpp.getOrDefault(it,0)+1);
         }
         int i=0,c=0;
-        for(int key:mpp.keySet()){
-            if(mpp.get(key)==2){
-                c+=1;
-            }
-        }
-        int[] ans=new int[c];
+        // for(int key:mpp.keySet()){
+        //     if(mpp.get(key)==2){
+        //         c+=1;
+        //     }
+        // }
+        int[] ans=new int[2];
         for(int key:mpp.keySet()){
             if(mpp.get(key)==2){
                 ans[i++]=key;
